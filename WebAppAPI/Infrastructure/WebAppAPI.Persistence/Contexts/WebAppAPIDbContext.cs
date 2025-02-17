aae5d612b239596233c3f6b33699d2c990b2e1f9
cemkdev <79233363+cemkdev@users.noreply.github.com>
2025-02-17 12:00:24 +0300

    commit 2: 1. The code-first approach has been applied. 2. Entities have been basically created. 3. The context object has been created.

---
+++ WebAppAPI/Infrastructure/WebAppAPI.Persistence/Contexts/WebAppAPIDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAppAPI.Domain.Entities;
+
+namespace WebAppAPI.Persistence.Contexts
+{
+    // It will be placed in the IoC because we need to be able to access it from everywhere.
+    public class WebAppAPIDbContext : DbContext
+    {
+        public WebAppAPIDbContext(DbContextOptions options) : base(options)
+        {
+            // It will process in the IoC...
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
